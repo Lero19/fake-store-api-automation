124cc74f9ff6e24860573aa711a5c845a214eb0a
leroy19 <N.Ncongwane@multichoicegroup.onmicrosoft.com>
2026-06-19 12:54:59 +0200

    remove duplicate allure logging from step definitions

--- src/test/java/stepdefinitions/CartSteps.java
+++ src/test/java/stepdefinitions/CartSteps.java
@@ -6,7 +6,6 @@ import io.cucumber.java.en.When;
 import io.qameta.allure.Allure;
 import io.restassured.response.Response;
 import services.CartService;
-import utils.AllureUtils;
 import validators.CartValidator;
 
 public class CartSteps {
@@ -18,17 +17,18 @@ public class CartSteps {
     @When("I request all carts")
     public void getAllCarts() {
         Allure.step("Requesting all carts");
+
         Response response = cartService.getAllCarts();
         context.setResponse(response);
-        AllureUtils.attachResponse(response);
     }
 
     @When("I request cart with id {int}")
     public void getCartById(int id) {
+
         Response response = cartService.getCartById(id);
         context.setResponse(response);
-        AllureUtils.attachResponse(response);
     }
+
     @Then("response should contain list of carts")
     public void verifyAllCarts() {
         validator.verifyAllCarts(context.getResponse());
@@ -42,10 +42,9 @@ public class CartSteps {
     @When("I create a new cart")
     public void createCart() {
         Allure.step("Creating a new cart");
+
         Response response = cartService.createCart();
         context.setResponse(response);
-        AllureUtils.attachResponse(response);
-
     }
 
     @Then("cart should be created successfully")
@@ -53,11 +52,12 @@ public class CartSteps {
         Allure.step("Validating cart creation response");
         validator.verifyCartCreated(context.getResponse());
     }
+
     @When("I update cart with id {int}")
     public void updateCart(int id) {
+
         Response response = cartService.updateCart(id);
         context.setResponse(response);
-        AllureUtils.attachResponse(response);
     }
 
     @Then("cart should be updated successfully")
@@ -68,9 +68,9 @@ public class CartSteps {
     @When("I delete cart with id {int}")
     public void deleteCart(int id) {
         Allure.step("Deleting cart with id: " + id);
+
         Response response = cartService.deleteCart(id);
         context.setResponse(response);
-        AllureUtils.attachResponse(response);
     }
 
     @Then("cart should be deleted successfully")
